aa430bb1ad97dce5bb49ea842e11922be73a464e
Marlna <katerain45@gmail.com>
2018-08-19 22:54:52 +0300

    added section about app

--- app/src/main/java/com/jaspiersin/gradient/Activities/AboutActivity.java
+++ app/src/main/java/com/jaspiersin/gradient/Activities/AboutActivity.java
@@ -7,6 +7,6 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_about);
     }
 }
